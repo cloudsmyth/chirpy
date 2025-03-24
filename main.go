3d78f487ddadeaf2ce189e7d82a7f790e898e9f8
Spencer Smith <spenceralansmith@icloud.com>
2025-03-23 20:20:01 -0700

    New routes

--- main.go
+++ main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) incrementHits(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricShowHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	metric := cfg.fileServerHits.Load()
+	metrics := fmt.Sprintf("Hits: %d\n", metric)
+	w.Write([]byte(metrics))
+}
+
+func (cfg *apiConfig) metricResetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits.Store(0)
+	w.Write([]byte("Counter reset to 0"))
+}
 
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	apiCfg := &apiConfig{}
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.incrementHits(handler))
+	mux.HandleFunc("GET /healthz", healthCheck)
+	mux.HandleFunc("GET /metrics", apiCfg.metricShowHandler)
+	mux.HandleFunc("POST /reset", apiCfg.metricResetHandler)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -23,4 +51,10 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Could not start server: %v", err)
 	}
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
